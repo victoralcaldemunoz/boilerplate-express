56d2d8b76f6864f534cd89e9c6dd8deec3426e92
victor <vimusic_92@hotmail.com>
2023-09-11 16:49:29 +0100

    serve an html file, serve a static assets

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ let express = require('express');
 let app = express();
 
 
+app.use('/public', express.static(__dirname + '/public'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
 
 
 
