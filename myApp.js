7f8535580d06c7d69ac1058948f1c5be9f22cf3e
victor <vimusic_92@hotmail.com>
2023-09-12 11:41:47 +0100

    get query parameter input from the client

--- myApp.js
+++ myApp.js
@@ -29,6 +29,10 @@ app.get('/:word/echo', (req, res) =>{
     res.json({ word: req.params.word })
 })
 
+app.get('/name', (req, res) =>{
+    res.json({ name: req.query.first + " " + req.query.last })
+})
+
 
 
 
