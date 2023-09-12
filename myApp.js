f394fac6d30e06e1c7dbdf67416ce1b2412c291b
victor <vimusic_92@hotmail.com>
2023-09-12 12:30:26 +0100

    get data from post request

--- myApp.js
+++ myApp.js
@@ -35,8 +35,9 @@ app.get('/name', (req, res) =>{
     res.json({ name: req.query.first + " " + req.query.last })
 })
 
-
-
+app.post('/name', (req, res) => {
+    res.json({ name: req.body.first + ' ' + req.body.last })
+})
 
 
 
